a073353a49a9ea28965e960289b23b9f1972cdfd
翟盈盈 <zhaiyingying@qiyi.com>
2018-08-03 13:00:24 +0800

    Site updated: 2018-08-03 13:00:24

--- js/totop.js
+++ js/totop.js
@@ -1,5 +1,11 @@
 $(window).scroll(function() {
     $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    var st = document.body.scrollTop || document.documentElement.scrollTop;
+    /*
+     * 在这里我给obj添加一个自定义属性。className可能会影响原有的class
+     * 三元运算使代码更简洁
+     */
+    $('#sidebar')[0].setAttribute("data-fixed",st >= 160?"fixed":"");
 });
 $("#rocket").click(function() {
     $("#rocket").addClass("launch");
